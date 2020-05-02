0baf80dd9f25d57e9043efedbf54441c75fd10fc
Fox Demo <demon5113@list.ru>
2020-05-02 14:01:12 +0300

    Create telegram bot translate

--- google_translate.py
+++ google_translate.py
@@ -1,10 +1,26 @@
+
+import telebot
 from googletrans import *
+import config
 
 #print(googletrans.LANGUAGES)
+bot = telebot.TeleBot("1297569871:AAEWP51O-YhcOr9KMUh0ZEDYAfaqCXnkE6Q")
 translator = Translator()
 
-while True:
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+	pass
+
+@bot.message_handler(content_types=['text'])
+def send_welcome(message):
+	bot.reply_to(message, translate_to_en(message.text))
+
+def translate_to_en(text):
     translator = Translator()
-    to_translate = input(">>")
-    result = translator.translate(to_translate, src = 'ru' ,dest='en')
-    print(result.text, end="\n")
+    result = translator.translate(text,  dest='en')
+    return result.text
+
+if __name__ == "__main__":
+    bot.polling()
+

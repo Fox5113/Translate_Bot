7395a7f7a0892ff45318fe5e93ce4ccc83833513
Fox Demo <demon5113@list.ru>
2020-05-02 23:07:40 +0300

    Fix

--- google_translate.py
+++ google_translate.py
@@ -1,10 +1,6 @@
-import configparser
 import telebot
-import emoji
-import regex
 import config
 from yandex_translate import YandexTranslate
-
 
 Token = config.Token
 bot = telebot.TeleBot(Token)
@@ -12,21 +8,21 @@ lang_to = "en"
 sess = {}
 
 
-
-
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-	bot.send_message(message.chat.id, "Для  выбора языка используйте слудующие команды.\n" +
-    "\\lang_to - для  установки языка на которого переводить. По умолчанию будет   использоватся английский язык ")
+    bot.send_message(message.chat.id, "Для  выбора языка используйте слудующие команды.\n" +
+                     "\\lang_to - для  установки языка на которого переводить. По умолчанию будет   использоватся "
+                     "английский язык ")
 
 
 @bot.message_handler(commands=['lang_to'])
 def handle_lang(message):
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='C английского на русский', switch_inline_query="en-ru", callback_data="en-ru"))
-    markup.add(telebot.types.InlineKeyboardButton(text='С русского на английский', switch_inline_query="ru-en", callback_data="ru-en"))
+    markup.add(telebot.types.InlineKeyboardButton(text='C английского на русский', switch_inline_query="en-ru",
+                                                  callback_data="en-ru"))
+    markup.add(telebot.types.InlineKeyboardButton(text='С русского на английский', switch_inline_query="ru-en",
+                                                  callback_data="ru-en"))
     bot.send_message(message.chat.id, "Выберите язык.", reply_markup=markup)
-
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -38,21 +34,21 @@ def callback_inline(call):
             sess[call.message.chat.id] = 'ru-en'
 
 
-
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
     bot.reply_to(message, translate_to_en(message.text, message.chat.id))
 
+
 def translate_to_en(text, user_id):
     translator = YandexTranslate(config.Yandex_api_key)
 
-
     if sess[user_id]:
-        result = translator.translate(text,  f"{sess[user_id]}")
+        result = translator.translate(text, f"{sess[user_id]}")
         return result['text']
     elif not sess[user_id]:
-        result = translator.translate(text,  f"{sess[user_id]}")
+        result = translator.translate(text, f"{sess[user_id]}")
         return result['text']
+
 
 if __name__ == "__main__":
     bot.polling()
